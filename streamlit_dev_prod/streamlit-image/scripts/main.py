4b776fbff27131f36a49a942f33fc458d6d91381
julien-data-analyst <julienrenoult76460@gmail.com>
2026-06-17 18:42:32 +0200

    feat(streamlit-docker-compose): added hello title into the streamlit app

---
+++ streamlit_dev_prod/streamlit-image/scripts/main.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+
+def main():
+   st.title("Welcome to Streamlit WebApp!")
+   st.write("This is an example of a basic Streamlit app.")
+
+if __name__ == "__main__":
+   main()
